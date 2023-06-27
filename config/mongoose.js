78ec586009dee3bfea86fd72bec9e4473f87959e
My java <ankit.sharma21598@gmail.com>
2023-06-27 17:08:52 +0530

    mongoose update

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // mongoose.connect('url') 
-mongoose.connect(process.env.DB_STRING);
+mongoose.connect(process.env.MONGO_URL);
 const db=mongoose.connection;
 
 //If any Error then Getting this Line
